49b5fc26ddd771307f876546cc4be10536a13b50
jean-barre <barre.jean.63@gmail.com>
2016-02-13 19:32:32 +0100

    Map v0.1

--- Spots/mobile/src/main/java/com/spots/MainActivity.java
+++ Spots/mobile/src/main/java/com/spots/MainActivity.java
@@ -1,9 +1,17 @@
 package com.spots;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
+import android.location.LocationProvider;
 import android.net.Uri;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -15,6 +23,14 @@ import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.spots.data.database.CategoryDB;
 import com.spots.data.database.SpotDB;
 import com.spots.data.model.Category;
@@ -24,15 +40,38 @@ import java.util.ArrayList;
 import java.util.List;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnMapReadyCallback {
 
     private Context mCtx;
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+        LatLng sydney = new LatLng(-33.867, 151.206);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            map.setMyLocationEnabled(true);
+            return;
+        }
+
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 13));
+
+        map.addMarker(new MarkerOptions()
+                .title("Sydney")
+                .snippet("Bonne chienne de Sydney.")
+                .position(sydney));
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mCtx = this;
+
+        // Gere la carte
+        MapFragment mapFragment = (MapFragment) getFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
 
         CategoryDB categoryDB = new CategoryDB(mCtx);
         List<Category> catList = categoryDB.getAll();
@@ -57,26 +96,7 @@ public class MainActivity extends Activity {
         String mDrawableName = "handbag";
         int resID = getResources().getIdentifier(mDrawableName , "drawable", getPackageName());
         imagev.setImageResource(resID);
-        /*
-        for( int i = 0; i < myLayout.getChildCount(); i++ )
-            if( myLayout.getChildAt( i ) instanceof EditText )
-                myEditTextList.add( (EditText) myLayout.getChildAt( i ) );
-        */
 
-
-        /*
-        SpotDB spotDB = new SpotDB(mCtx);
-        List<Spot> spotList = spotDB.getAll();
-        List<String> spotNameList = new ArrayList<>();
-
-        if (spotList != null) {
-            for (Spot spot : spotList) {
-                spotNameList.add(spot.getName());
-            }
-            ListView listView = (ListView)findViewById(R.id.list_category);
-            listView.setAdapter(new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1 , spotNameList));
-        }
-        */
     }
 
     public void addTestSpots(View view) {
@@ -94,6 +114,56 @@ public class MainActivity extends Activity {
         Intent intent = new Intent(this, SavedPlaces.class);
         startActivity(intent);
     }
+/*
+    public void startMap() {
+        // INIT MAP
+        LocationManager locationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+        ArrayList<LocationProvider> providers = new ArrayList<LocationProvider>();
+        ArrayList<String> names = (ArrayList)locationManager.getProviders(true);
+
+        for(String name : names)
+            providers.add(locationManager.getProvider(name));
+
+        Criteria critere = new Criteria();
+        critere.setAccuracy(Criteria.ACCURACY_FINE);
+        critere.setAltitudeRequired(false);
+        critere.setBearingRequired(true);
+        critere.setCostAllowed(false);
+        critere.setPowerRequirement(Criteria.POWER_HIGH);
+        critere.setSpeedRequired(false);
+
+        String provider = locationManager.getBestProvider(critere, true);
+        LocationListener locationListener = new LocationListener() {
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+                Log.d("chien", "ta mère");
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+
+            @Override
+            public void onLocationChanged(Location location) {
+                Log.d("GPS", "Latitude " + location.getLatitude() + " et longitude " + location.getLongitude());
+            }
+        };
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 60000, 90, locationListener);
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            mMap.setMyLocationEnabled(true);
+        } else {
+            // Show rationale and request permission.
+        }
+
+    }*/
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
