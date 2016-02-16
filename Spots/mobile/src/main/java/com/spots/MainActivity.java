6ad649bd8ddbfdc98dcf4258cae57025e2f88afa
jean-barre <barre.jean.63@gmail.com>
2016-02-16 08:12:51 +0100

    WOOOOOOOOOOOOOOO

--- Spots/mobile/src/main/java/com/spots/MainActivity.java
+++ Spots/mobile/src/main/java/com/spots/MainActivity.java
@@ -1,5 +1,6 @@
 package com.spots;
 
+import android.app.Activity;
 import android.support.v4.app.NotificationManagerCompat;
 import android.support.v4.app.NotificationCompat;
 import android.app.Notification;
