5c3f619806ed9fc2943377fe7c2d1f3a9deb0642
jean-barre <barre.jean.63@gmail.com>
2016-02-29 15:15:33 +0100

    retour slider

--- Spots/wear/src/main/java/com/spots/MainActivity.java
+++ Spots/wear/src/main/java/com/spots/MainActivity.java
@@ -2,37 +2,50 @@ package com.spots;
 
 import android.app.Activity;
 import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.wearable.view.WatchViewStub;
 import android.util.Log;
-import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.wearable.DataApi;
 import com.google.android.gms.wearable.DataEventBuffer;
 import com.google.android.gms.wearable.Wearable;
 import com.spots.data.database.CategoryDB;
-import com.spots.data.model.Category;
 
-import java.util.List;
 
-public class MainActivity extends Activity implements DataApi.DataListener, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener{
+public class MainActivity extends Activity implements DataApi.DataListener,
+        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener,
+        LocationListener{
 
-    private String TAG = "MAIN_ACTIVITY";
+    private String TAG = "WEAR_MAIN_ACTIVITY";
+    public static String PACKAGE_NAME;
     private TextView mTextView;
-    LinearLayout categoryLayout;
+    public LinearLayout categoryLayout;
     private Context mCtx;
     private GoogleApiClient mGoogleApiClient;
+
+    public static final long UPDATE_INTERVAL_IN_MILLISECONDS = 10000;
+    public static final long FASTEST_UPDATE_INTERVAL_IN_MILLISECONDS =
+            UPDATE_INTERVAL_IN_MILLISECONDS / 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        PACKAGE_NAME = getPackageName();
+        final Activity mainActivity = this;
         final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
         stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
             @Override
@@ -41,11 +54,13 @@ public class MainActivity extends Activity implements DataApi.DataListener, Goog
                 categoryLayout = (LinearLayout) stub.findViewById(R.id.categoryLayout);
                 mCtx = getApplicationContext();
                 // We fill the list of categories with all our categories
-                fillCategoryList();
+                CategoryDB catDB = new CategoryDB(mCtx);
+                catDB.fillCategoryList(mainActivity, mCtx,categoryLayout);
             }
         });
 
         mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(LocationServices.API)
                 .addApi(Wearable.API)
                 .addConnectionCallbacks(this)
                 .addOnConnectionFailedListener(this)
@@ -62,11 +77,39 @@ public class MainActivity extends Activity implements DataApi.DataListener, Goog
 
     @Override
     public void onConnected(@Nullable Bundle bundle) {
+        Log.d(TAG,"on connected");
         Wearable.DataApi.addListener(mGoogleApiClient, this);
+        LocationRequest locationRequest = LocationRequest.create()
+                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+                .setInterval(UPDATE_INTERVAL_IN_MILLISECONDS)
+                .setFastestInterval(FASTEST_UPDATE_INTERVAL_IN_MILLISECONDS);
+
+        LocationServices.FusedLocationApi
+                .requestLocationUpdates(mGoogleApiClient, locationRequest, this)
+                .setResultCallback(new ResultCallback() {
+
+                    @Override
+                    public void onResult(Status status) {
+                        if (status.getStatus().isSuccess()) {
+                            if (Log.isLoggable(TAG, Log.DEBUG)) {
+                                Log.d(TAG, "Successfully requested location updates");
+                            }
+                        } else {
+                            Log.e(TAG,
+                                    "Failed in requesting location updates, "
+                                            + "status code: "
+                                            + status.getStatusCode()
+                                            + ", message: "
+                                            + status.getStatusMessage());
+                        }
+                    }
+                });
+
     }
 
     @Override
     protected void onPause() {
+        Log.d(TAG,"on pause");
         super.onPause();
         Wearable.DataApi.removeListener(mGoogleApiClient, this);
         mGoogleApiClient.disconnect();
@@ -74,7 +117,7 @@ public class MainActivity extends Activity implements DataApi.DataListener, Goog
 
     @Override
     public void onConnectionSuspended(int i) {
-
+        Log.d(TAG,"on connection suspended");
     }
 
     @Override
@@ -84,30 +127,33 @@ public class MainActivity extends Activity implements DataApi.DataListener, Goog
 
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-
-    }
-
-    private void fillCategoryList() {
-
-        Category category;
-        CategoryDB categoryDB = new CategoryDB(mCtx);
-        List<Category> categoryList = categoryDB.getAll();
-        String[] namesArray = new String[categoryList.size()];
-        int[] imagesArray = new int[categoryList.size()];
-
-        for (int i = 0; i < categoryList.size(); i++) {
-            category = categoryList.get(i);
-            namesArray[i] = category.getName();
-            imagesArray[i] = getResources().getIdentifier(category.getLogo(),"drawable",getPackageName());
-        }
-
-        CategoryListAdapter adapter = new CategoryListAdapter(this, mCtx, imagesArray, namesArray);
-        final int adapterCount = adapter.getCount();
-        for (int i = 0; i < adapterCount; i++) {
-            View item = adapter.getView(i, null, null);
-            categoryLayout.addView(item);
-        }
+        Log.d(TAG,"on connection failed");
     }
 
 
+    /*
+    ***************************************************************************************************
+                                            LOCATION MANAGEMENT
+    ***************************************************************************************************
+     */
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
 }
